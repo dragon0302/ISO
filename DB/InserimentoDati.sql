970c5323bcdd17f5b0dd8daca376c93d60572510
Vix <v.ruotolo4@studenti.unisa.it>
2025-04-01 13:21:17 +0200

    Completato DAO

--- DB/InserimentoDati.sql
+++ DB/InserimentoDati.sql
@@ -1,4 +1,4 @@
-INSERT INTO Utente (CF, NomeUtente, Password, Nome, Cognome, Sesso, DataNascita, Amministratore)
+INSERT INTO utente (CF, NomeUtente, Password, Nome, Cognome, Sesso, DataNascita, Amministratore)
 VALUES (?,?,?,?,?,?,?,?);
 
 INSERT INTO carrello (ID_carrello, CF_utente)
@@ -12,9 +12,6 @@ VALUES (?,?,?,?,?,?);
 
 INSERT INTO ordine (ID_ordine, Data_ordine, Prezzo_tot, ID_carrello)
 VALUES (?,?,?,?);
-
-INSERT INTO indirizzo (CF_Utente, ID_indirizzo, città, Provincia, CAP, Via, Civico, Indirizzo2, Note, Fatturazione)
-VALUES (?,?,?,?,?,?,?,?,?,?);
 
 INSERT INTO indirizzo (CF_Utente, ID_indirizzo, città, Provincia, CAP, Via, Civico, Scala, Indirizzo2, Note, Fatturazione)
 VALUES (?,?,?,?,?,?,?,?,?,?,?);
