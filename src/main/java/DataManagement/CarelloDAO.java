0c2458478677578036dbc8e546cd0b222983c9aa
carmine boninfante <132886062+dragon0302@users.noreply.github.com>
2025-04-01 11:23:24 +0200

    creazione del dao e la sua implementazzione per la classe carello

---
+++ src/main/java/DataManagement/CarelloDAO.java
@@ -0,0 +1,9 @@
+package DataManagement;
+
+import java.sql.SQLException;
+
+public interface CarelloDAO {
+
+    public void DoSave(Carrello carello) throws SQLException;
+
+}
---
+++ src/main/java/DataManagement/CarelloDAOImplement.java
@@ -0,0 +1,49 @@
+package DataManagement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CarelloDAOImplement {
+
+    private static DataSource ds;
+    private static final String TABLE_NAME = "carello";
+
+    static {
+        try {
+            Context inixCtx = new InitialContext();
+            Context envCtx = (Context) inixCtx.lookup("java:/comp/env");
+
+            ds = (DataSource) envCtx.lookup("jdbc/ISO16");
+        } catch (NamingException e) {
+            System.out.println("Naming Exception: " + e.getMessage());
+        }
+    }
+
+    public synchronized void DoSave(Carrello carrello) throws SQLException {
+        Connection conn = null;
+        PreparedStatement query = null;
+
+        try {
+            conn = ds.getConnection();
+            query = conn.prepareStatement("INSERT INTO" + TABLE_NAME +  "(ID_carrello, CF_utente) VALUES (?,?)");
+
+            query.executeUpdate();
+        }finally {
+            try {
+                if (query != null) {
+                    query.close();
+                }
+            }finally {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+        }
+    }
+
+}
