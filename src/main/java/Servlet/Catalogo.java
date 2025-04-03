7d2fbea89c7802e13a2e88311f49aacf9a28c986
carmine boninfante <132886062+dragon0302@users.noreply.github.com>
2025-04-03 11:26:57 +0200

    Correzione Servlet Catalogo

---
+++ src/main/java/Servlet/Catalogo.java
@@ -0,0 +1,34 @@
+package Servlet;
+
+import DataManagement.Prodotto;
+import DataManagement.ProdottoDAO;
+import DataManagement.ProdottoDAOImplement;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/Catalogo")
+public class Catalogo extends HttpServlet {
+    ProdottoDAO prodottoDAO = new ProdottoDAOImplement();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            List<Prodotto> prodotti = prodottoDAO.getProdotti();
+
+            request.setAttribute("prodotti", prodotti);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/Home.jsp");
+            dispatcher.forward(request, response);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
