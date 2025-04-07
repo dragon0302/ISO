a131d470d192af325404f4442422cc79a3f9df46
carmine boninfante <132886062+dragon0302@users.noreply.github.com>
2025-04-07 21:04:39 +0200

    Aggunta funzione che ritorna gli id dei prodotti più venduti

--- src/main/java/DataManagement/AcquistoDAOImplementi.java
+++ src/main/java/DataManagement/AcquistoDAOImplementi.java
@@ -6,7 +6,9 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class AcquistoDAOImplementi implements AcquistoDAO {
 
@@ -16,7 +18,7 @@ public class AcquistoDAOImplementi implements AcquistoDAO {
     static {
         try {
             Context inixContext = new InitialContext();
-            Context envContext = (Context) inixContext.lookup("java:/comp/anv");
+            Context envContext = (Context) inixContext.lookup("java:/comp/env");
 
             ds = (DataSource) envContext.lookup("jdbc/ISO16");
         }catch (NamingException e){
@@ -52,6 +54,37 @@ public class AcquistoDAOImplementi implements AcquistoDAO {
                 }
             }
         }
+    }
+
+    public ArrayList<Integer> getProdottiPiuAqquistati() throws SQLException{
+
+        Connection conn = null;
+        PreparedStatement query2 = null;
+        ArrayList<Integer> prodottiPiuAqquistati = new ArrayList<>();
+
+        try {
+            conn = ds.getConnection();
+            query2 = conn.prepareStatement("select distinct ID_prodotto from " + TABLE_NAME + " where Quantità >= 10");
+
+            ResultSet rs = query2.executeQuery();
+
+            while (rs.next()) {
+                prodottiPiuAqquistati.add(rs.getInt(1));
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (query2 != null) {
+                    query2.close();
+                }
+            } finally {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+        }
+        return prodottiPiuAqquistati;
     }
 
 }
