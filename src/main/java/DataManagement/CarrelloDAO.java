540fbea5ff2617a14d194dbd089c981fb0e8cdb0
carmine boninfante <132886062+dragon0302@users.noreply.github.com>
2025-04-28 20:11:14 +0200

    aggunte funzioni per settare i prodotti nel carello e per avere l'id del carello di uno specifico utente

--- src/main/java/DataManagement/CarrelloDAO.java
+++ src/main/java/DataManagement/CarrelloDAO.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 public interface CarrelloDAO {
 
     public void DoSave(Carrello carello) throws SQLException;
-    public void ProdottiCarello(int idProdotto,int idCarrello) throws SQLException;
+    public void GetProdottiCarello(int idProdotto,int idCarrello) throws SQLException;
+    public void SetProdottiCarello(String idProdotti,String idCarello) throws SQLException;
+    public String GetIdCarello(String CF_utente) throws SQLException;
 
 }
--- src/main/java/DataManagement/CarrelloDAOImplement.java
+++ src/main/java/DataManagement/CarrelloDAOImplement.java
@@ -6,6 +6,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CarrelloDAOImplement implements CarrelloDAO {
@@ -50,7 +51,7 @@ public class CarrelloDAOImplement implements CarrelloDAO {
         }
     }
 
-    public void ProdottiCarello(int idProdotto,int idCarrello) throws SQLException{
+    public void GetProdottiCarello(int idProdotto,int idCarrello) throws SQLException{
 
         Connection conn = null;
         PreparedStatement query2 = null;
@@ -77,6 +78,66 @@ public class CarrelloDAOImplement implements CarrelloDAO {
             }
         }
 
+    }
+
+    public void SetProdottiCarello(String idProdotti,String idCarello) throws SQLException {
+
+        Connection conn = null;
+        PreparedStatement query3 = null;
+
+        try {
+            conn = ds.getConnection();
+            query3 = conn.prepareStatement("update " + TABLE_NAME + " set Lista_prodotti = ? where ID_carrello = ?");
+
+            query3.setString(1,idProdotti);
+            query3.setString(2,idCarello);
+
+            query3.executeUpdate();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (query3 != null) {
+                    query3.close();
+                }
+            }finally {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+        }
+
+    }
+
+    public String GetIdCarello(String CF_utente) throws SQLException{
+        Connection conn = null;
+        PreparedStatement query4 = null;
+        String idCarello = null;
+
+        try {
+            conn = ds.getConnection();
+            query4 = conn.prepareStatement("SELECT ID_carrello from " + TABLE_NAME + " WHERE CF_utente = ?");
+
+            query4.setString(1,CF_utente);
+            ResultSet rs = query4.executeQuery();
+
+            if (rs.next()) {
+                idCarello = String.valueOf(rs.getInt(1));
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (query4 != null) {
+                    query4.close();
+                }
+            }finally {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+        }
+        return idCarello;
     }
 
 }
