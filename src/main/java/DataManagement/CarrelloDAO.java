12e8dcb8378afc9b76b334dbfb49ea97ba965107
carmine boninfante <132886062+dragon0302@users.noreply.github.com>
2025-05-04 19:32:59 +0200

    aggunte funzioni che aggiornano la lista di prodotti nel carello e funzione che ritorna l'id del carello

--- src/main/java/DataManagement/CarrelloDAO.java
+++ src/main/java/DataManagement/CarrelloDAO.java
@@ -1,10 +1,13 @@
 package DataManagement;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface CarrelloDAO {
 
     public void DoSave(Carrello carello) throws SQLException;
     public void ProdottiCarello(int idProdotto,int idCarrello) throws SQLException;
+    public void UpdateProductCarello(List<String> idsProduct, String CF) throws SQLException;
+    public Integer GetIdCarrello(String CF) throws SQLException;
 
 }
