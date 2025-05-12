d41b677b2b97c2c995532948f9f05fae401d4387
carmine boninfante <132886062+dragon0302@users.noreply.github.com>
2025-05-12 14:20:01 +0200

    aggunta prodotti al carello per l'utete reggistarto

--- src/main/java/Servlet/ProductCartMenegment.java
+++ src/main/java/Servlet/ProductCartMenegment.java
@@ -1,5 +1,6 @@
 package Servlet;
 
+import DataManagement.*;
 import Utility.CookieManagemnt;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,21 +11,34 @@ import java.io.IOException;
 @WebServlet("/ProductCartMenegment")
 public class ProductCartMenegment extends HttpServlet {
 
+    CarrelloDAO carrelloDAO = new CarrelloDAOImplement();
+    AcquistoDAO acquistoDAO = new AcquistoDAOImplement();
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         try {
 
             String prodottoID = request.getParameter("prodottoID");
             String sourcePage = request.getParameter("SourcePage");
+            Utente utente = (Utente) request.getSession().getAttribute("utente");
             CookieManagemnt cm = new CookieManagemnt(request);
             String redirectPage = null;
             Boolean contenuto = true;
 
-            if(prodottoID != null) {
-                cm.CookieCartManagemnt(response, prodottoID);
+            if(utente == null) {
+                if (prodottoID != null) {
+                    cm.CookieCartManagemnt(response, prodottoID);
+                }
+            }else {
+
+                if (!carrelloDAO.CeckProdotto(Integer.parseInt(prodottoID),utente.getCf())) {
+                    carrelloDAO.ProdottiCarello(Integer.parseInt(prodottoID), carrelloDAO.GetIdCarrello(utente.getCf()));
+                    Acquisto acquisto = new Acquisto(false,1, carrelloDAO.GetIdCarrello(utente.getCf()),Integer.parseInt(prodottoID));
+                    acquistoDAO.DoSave(acquisto);
+                }else {
+                    acquistoDAO.UpdateQuantity(Integer.parseInt(prodottoID), carrelloDAO.GetIdCarrello(utente.getCf()));
+                }
             }
-
-
 
             if("Prodotto".equals(sourcePage)) {
                 redirectPage = "ProdottoS?id=" + prodottoID;
