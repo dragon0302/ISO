2472bea0ece020a06984e0434ea25f0f43122998
vittruot <v.ruotolo4@studenti.unisa.it>
2025-07-08 10:13:35 +0200

    implementato MetodoPagamentoDAO

--- src/main/java/DataManagement/MetodoPagamentoDAO.java
+++ src/main/java/DataManagement/MetodoPagamentoDAO.java
@@ -1,7 +1,12 @@
 package DataManagement;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
 public interface MetodoPagamentoDAO {
     public void doSave(MetodoPagamento metodopagamento) throws SQLException;
+    public double getNumeroCarta(String CF_Utente) throws SQLException;
+    public Date getDataScadenza(String CF_Utente) throws SQLException;
+    public double getCVV(String CF_Utente) throws SQLException;
+    public String getTipo(String CF_Utente) throws SQLException;
 }
--- src/main/java/DataManagement/MetodoPagamentoDAOImplement.java
+++ src/main/java/DataManagement/MetodoPagamentoDAOImplement.java
@@ -4,9 +4,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MetodoPagamentoDAOImplement implements MetodoPagamentoDAO {
     private static DataSource ds;
@@ -27,8 +25,8 @@ public class MetodoPagamentoDAOImplement implements MetodoPagamentoDAO {
         PreparedStatement query = null;
         try {
             conn = ds.getConnection();
-            query = conn.prepareStatement("INSERT INTO" + TABLE_NAME +  " (CF_Utente, NumeroCarta, DataScadenza, CVV, Tipo, Default_pagamento) VALUES (?,?,?,?,?,?)");
-            query = conn.prepareStatement("INSERT INTO" + TABLE_NAME +  " (CF_Utente, NumeroCarta, DataScadenza, CVV, Tipo, Default_pagamento) VALUES (?,?,?,?,?,?)");
+            query = conn.prepareStatement("INSERT INTO " + TABLE_NAME +  " (CF_Utente, NumeroCarta, DataScadenza, CVV, Tipo, Default_pagamento) VALUES (?,?,?,?,?,?)");
+            query = conn.prepareStatement("INSERT INTO " + TABLE_NAME +  " (CF_Utente, NumeroCarta, DataScadenza, CVV, Tipo, Default_pagamento) VALUES (?,?,?,?,?,?)");
             query.setString(1,metodopagamento.getCF_utente());
             query.setDouble(2,metodopagamento.getNumerocarta());
             //query.setDate(3,metodopagamento.getDataScadenza());
@@ -47,6 +45,80 @@ public class MetodoPagamentoDAOImplement implements MetodoPagamentoDAO {
                 }
             }
         }
+    }
+    public double getNumeroCarta(String CF_Utente) throws SQLException{
+        double result = 0;
+        Connection conn = null;
+        PreparedStatement query = null;
+        ResultSet rs = null;
+        try{
+            conn = ds.getConnection();
+            query = conn.prepareStatement("SELECT NumeroCarta FROM " + TABLE_NAME + " WHERE CF_Utente = ?");
+            query.setString(1,CF_Utente);
+            rs = query.executeQuery();
+            if(rs.next()){
+                result = rs.getDouble(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public Date getDataScadenza(String CF_Utente) throws SQLException{
+        Date result = null;
+        Connection conn = null;
+        PreparedStatement query = null;
+        ResultSet rs = null;
+        try{
+            conn = ds.getConnection();
+            query = conn.prepareStatement("SELECT DataScadenza FROM " + TABLE_NAME + " WHERE CF_Utente = ?");
+            query.setString(1,CF_Utente);
+            rs = query.executeQuery();
+            if(rs.next()){
+                result = rs.getDate(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public double getCVV(String CF_Utente) throws SQLException{
+        double result = 0;
+        Connection conn = null;
+        PreparedStatement query = null;
+        ResultSet rs = null;
+        try{
+            conn = ds.getConnection();
+            query = conn.prepareStatement("SELECT CVV FROM " + TABLE_NAME + " WHERE CF_Utente = ?");
+            query.setString(1,CF_Utente);
+            rs = query.executeQuery();
+            if(rs.next()){
+                result = rs.getDouble(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    public String getTipo(String CF_Utente) throws SQLException {
+        String result = "";
+        Connection conn = null;
+        PreparedStatement query = null;
+        ResultSet rs = null;
+        try{
+            conn = ds.getConnection();
+            query = conn.prepareStatement("SELECT Tipo FROM " + TABLE_NAME + " WHERE CF_Utente = ?");
+            query.setString(1,CF_Utente);
+            rs = query.executeQuery();
+            if(rs.next()){
+                result = rs.getString(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return result;
     }
 }
 
